38ef3d82a0eed2323eb4815f173463cd41ecb2f2
trwelk <weerasooriya.trewon@gmail.com>
2021-08-18 18:49:16 +0530

    bugfic in inv reducer

--- frontend/src/redux/reducers/InventoryReducer.js
+++ frontend/src/redux/reducers/InventoryReducer.js
@@ -3,7 +3,8 @@ import { ActionTypes } from "../constants/ActionTypes";
 const initialState = {
     inventory: [],
     isLoading: false,
-    error: null
+    error: null,
+    parentLocation:""
 }
 export const InventoryReducer = (state = initialState, { type, payload }) => {
     switch (type) {
@@ -14,13 +15,17 @@ export const InventoryReducer = (state = initialState, { type, payload }) => {
         case ActionTypes.CREATE_INVENTORY_SUCCESS:
             return { ...state, inventory: [...state.inventory, payload] }
         case ActionTypes.UPDATE_INVENTORY_SUCCESS:
-            let oldInventory = state.workshops
+            let oldInventory = state.inventory
             let newInventory = oldInventory.filter(item => item.id !== payload.id)
             newInventory.push(payload)
             return { ...state, inventory: newInventory }
         case ActionTypes.DELETE_INVENTORY_SUCCESS:
             let remainingInventory = state.inventory.filter(item => item.id !== payload)
             return { ...state, inventory: remainingInventory }
+        case ActionTypes.INVENTORY_LOCATION_SELECTED:
+                return { ...state, parentLocation: payload }
+
+
         default:
             return state;
     }
