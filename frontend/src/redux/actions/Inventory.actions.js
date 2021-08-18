167b549f2be0639256958bc5ef230d7ef8e343d4
trwelk <weerasooriya.trewon@gmail.com>
2021-08-18 18:49:07 +0530

    redux impl for inv parts

---
+++ frontend/src/redux/actions/Inventory.actions.js
@@ -0,0 +1,83 @@
+import axios from 'axios'
+import {ActionTypes} from  '../constants/ActionTypes'
+import {AppConstants} from '../constants/AppConstants'
+
+export const fetchInventorysSuccess = (data) =>  {
+    return {
+        type: ActionTypes.FETCH_INVENTORY_SUCCESS,
+        payload: data
+    }
+}
+
+
+export const fetchInventories = (dispatch) => {
+        axios.get(AppConstants.REST_URL_HOST + AppConstants.INVENTORY_URL)
+            .then(response => {
+                dispatch(fetchInventorysSuccess(response.data))
+            })
+            .catch(error => {
+                console.log(error)
+            }) 
+}
+
+//UPDATE
+export const updateInventory = (dispatch, inventory) => {
+    axios.put(AppConstants.REST_URL_HOST + AppConstants.INVENTORY_URL, inventory)
+        .then(response => {
+            console.log(response)
+            dispatch(updateInventorySuccess({...response.data}))
+        })
+        .catch(error => {
+            console.log(error)
+        }) 
+}
+
+export const updateInventorySuccess = (data) =>  {
+return {
+    type: ActionTypes.UPDATE_INVENTORY_SUCCESS,
+    payload: data
+}
+}
+
+//CREATE
+export const createInventory = (data,dispatch) => {
+    const inventory = data;
+    console.log(inventory)
+    axios.post(AppConstants.REST_URL_HOST + AppConstants.INVENTORY_URL, inventory )
+        .then(response => {
+
+            const id = response;
+                    dispatch(createInventorySuccess(response.data))
+        })
+        .catch(error => {
+            console.log(error)
+
+        })
+
+}
+
+export const createInventorySuccess = (data) => {
+    return {
+        type: ActionTypes.CREATE_INVENTORY_SUCCESS  ,
+        payload: data
+    }
+}
+
+//DELETE
+export const deleteInventory = (dispatch, inventoryId) => {
+        axios.delete(AppConstants.REST_URL_HOST + AppConstants.INVENTORY_URL + '/' + inventoryId)
+            .then(response => {
+                console.log(response)
+                dispatch(deleteInventorySuccess( inventoryId ))
+            })
+            .catch(error => {
+                console.log(error)
+            }) 
+}
+
+export const deleteInventorySuccess = (data) =>  {
+    return {
+        type: ActionTypes.DELETE_INVENTORY_SUCCESS,
+        payload: data
+    }
+}
--- frontend/src/redux/reducers/InventoryLocationReducer.js
+++ frontend/src/redux/reducers/InventoryLocationReducer.js
@@ -1,5 +1,4 @@
 import { ActionTypes } from "../constants/ActionTypes";
-
 const initialState = {
     inventoryLocations: [],
     isLoading: false,
@@ -12,14 +11,22 @@ export const InventoryLocationReducer = (state = initialState, { type, payload }
             return { ...state, inventoryLocations: payload }
         case ActionTypes.INVENTORY_LOCATION_SELECTED:
             const selectedLocation = state.inventoryLocations.filter(location => {
-                return location.locationName === payload
+                return location.id === payload
             })[0];
-            console.log(selectedLocation)
-            if(selectedLocation)
+            if (selectedLocation)
                 return { ...state, selectedInventory: selectedLocation }
             else
                 return { ...state, selectedInventory: null }
-
+        case ActionTypes.DELETE_LOCATION_SUCCESS:
+            let remainingLocations = state.inventoryLocations.filter(item => item.id !== payload)
+            return { ...state, inventoryLocations: remainingLocations }
+        case ActionTypes.CREATE_LOCATION_SUCCESS:
+            return { ...state, inventoryLocations: [...state.inventoryLocations, payload] }
+        case ActionTypes.UPDATE_LOCATION_SUCCESS:
+            let oldLocation = state.inventoryLocations
+            let newLocation = oldLocation.filter(item => item.id !== payload.id)
+            newLocation.push(payload)
+            return { ...state, inventoryLocations: newLocation }
         default:
             return state;
     }
