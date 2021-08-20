55e7ed85edbdefe44c99728087211ee758bbb8fc
trwelk <weerasooriya.trewon@gmail.com>
2021-08-20 19:54:34 +0530

    add actions to call api for suppliers

--- frontend/src/redux/reducers/Index.js
+++ frontend/src/redux/reducers/Index.js
@@ -2,12 +2,14 @@ import {combineReducers} from 'redux'
 import { InventoryReducer } from './InventoryReducer'
 import { InventoryLocationReducer } from './InventoryLocationReducer'
 import { ProductReducer } from './ProductReducer'
+import { SupplierReducer } from './SupplierReducer'
 
 
  const reducer = combineReducers({
     inventoryReducer: InventoryReducer,
     inventoryLocationReducer:InventoryLocationReducer,
-    productReducer:ProductReducer
+    productReducer:ProductReducer,
+    supplierReducer:SupplierReducer
 })
 
 export default reducer
