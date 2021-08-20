216b34bb5900991912a28789032c3415c91bdb8e
trwelk <weerasooriya.trewon@gmail.com>
2021-08-20 19:52:25 +0530

    changes to app constants

--- frontend/src/constants/AppConstants.js
+++ frontend/src/constants/AppConstants.js
@@ -1,8 +1,9 @@
 export const AppConstants = {
     REST_URL_HOST : "https://icafbackend.azurewebsites.net",
     REST_URL_HOST : "http://localhost:9090",
-    STATE_APPROVED: "approved",
-    STATE_REQUESTED: "requested",
-    STATE_DECLINED: "declined",
+    STATE_APPROVED: "APPROVED",
+    STATE_REQUESTED: "REQUESTED",
+    STATE_DECLINED: "DECLINED",
+    STATE_REMOVED: "REMOVED",
     DUMMY: "dummy"
 };
