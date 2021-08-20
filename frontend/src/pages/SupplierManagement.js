b69fda7fa3e776492f0785a6227e712df5667f08
trwelk <weerasooriya.trewon@gmail.com>
2021-08-20 19:52:15 +0530

    add supplier management interface

---
+++ frontend/src/pages/SupplierManagement.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import InsertSupplierForm from '../components/supplier/InsertSupplierForm';
+function SupplierManagement() {
+    
+    return (
+        <div>
+            <InsertSupplierForm/>
+        </div>
+
+    );
+}
+
+export default SupplierManagement;
