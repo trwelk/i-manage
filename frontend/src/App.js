93b742289dc47d7b5665bb9df7f19180a6f22da8
IT19163864 <64912486+ryanmlk@users.noreply.github.com>
2021-08-21 21:18:01 +0530

    Added navbar and changed theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-import { useSelector } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import InventoryPartTable from './components/inventory/InventoryPartTable';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import InventoryItemsInLocationPage from './pages/InventoryItemsInLocationPage';
 import ProductManagement from './pages/ProductManagement';
 import LocationManagement from './pages/LocationManagement';
 import SupplierInsights from './pages/SupplierInsights';
 import SupplierManagement from './pages/SupplierManagement';
+import LandingPage from './pages/LandingPage';
+import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#5c5c5c',
+      main: '#333333',
+      dark: '#0c0c0c',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ffbdaf',
+      main: '#ff8b80',
+      dark: '#c85b53',
+      contrastText: '#000',
+    },
+  },
+});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
        <Router>
         <Switch>
+        <Route exact path="/" component={LandingPage} />
           <Route exact path="/inventory" component={InventoryItemsInLocationPage} />
           <Route exact path="/products" component={ProductManagement} />
           <Route exact path="/locations" component={LocationManagement} />
@@ -22,6 +41,7 @@ function App() {
         <Route exact path="/suppliers" component={SupplierManagement} />
       </Router>
     </div>
+    </ThemeProvider>
   );
 }
 
