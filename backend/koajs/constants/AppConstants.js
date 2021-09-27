a1fd5ee0b049fe815e6cf993665534fa1d291224
IT19163864 <64912486+ryanmlk@users.noreply.github.com>
2021-09-27 20:40:13 +0530

    Changed db connection url

--- backend/koajs/constants/AppConstants.js
+++ backend/koajs/constants/AppConstants.js
@@ -1,6 +1,6 @@
 const AppConstants = {
     HOST_URL : "cluster0-shard-00-01.ijxvo.mongodb.net:27017/iCaf",
-    DB_URL: "mongodb://ryan-testdb:XBKNOg0BMVPuEKudh8HowRUfnzrxtEGNmWbNIwJP281GxmcvOGcB6O62IkqAiniqhtbgjxxqKRQT19N9TgnNpg==@ryan-testdb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@ryan-testdb@",
+    DB_URL: "mongodb+srv://root:BwXwa9Af3H_aMUB@ryanprojcluster.v3mjk.mongodb.net/spm_db?retryWrites=true&w=majority",
     STATE_APPROVED: "APPROVED",
     STATE_REQUESTED: "REQUESTED",
     STATE_DECLINED: "DECLINED"
